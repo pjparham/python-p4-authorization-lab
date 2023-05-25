4c86643b227e08a22c94b27be27b372293bf50cf
Parker Parham <parkerparham@gmail.com>
2023-05-25 16:58:13 -0400

    Adds functionality to ensure users are logged in before provididing certain content

--- server/app.py
+++ server/app.py
@@ -18,6 +18,19 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.before_request
+def check_if_member():
+    open_access_list = [
+        'clear',
+        'article_list',
+        'show_article',
+        'login',
+        'logout',
+        'check_session'
+    ]
+    if (request.endpoint) not in open_access_list and (not session.get('user_id')):
+        return make_response({'error': 'Unauthorized'}, 401)
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -87,12 +100,14 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        articles = Article.query.filter(Article.is_member_only == True).all()
+        return make_response([article.to_dict() for article in articles], 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first().to_dict()
+        return make_response(article, 200)
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
